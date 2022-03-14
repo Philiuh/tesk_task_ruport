6cea3a6d63120a7ee693aef961d09a4e2aeddf94
philiuh <piliuh3@gmail.com>
2022-03-14 12:23:08 +0300

    wtf

--- dev/res/js-dev/components/filter/selectCards.js
+++ dev/res/js-dev/components/filter/selectCards.js
@@ -1,15 +1,15 @@
 import Choices from 'choices.js';
-import 'choices.js/src/styles/choices.scss';
-
-let select;
 
 const shouldRun = () => 1;
 
 const findElements = () => {
-  const element = document.querySelector('.menu-select');
-  console.log(element);
-  select = new Choices(element, {
+  const element = document.querySelector('.menu__select');
+  const select = new Choices(element, {
     searchEnabled: false,
+    itemSelectText: '',
+    classNames: {
+      focusState: '',
+    },
   });
 };
 
